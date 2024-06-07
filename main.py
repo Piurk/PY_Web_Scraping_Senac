251fe24cc694dd285a1dbcf23e10f8d03d86ba2d
Xirou <xirouzin@gmail.com>
2024-06-07 18:00:17 -0300

    Atualização Codigo
    Adição da Biblioteca Unidecode
    
    Adição do código "TESTE COM BS", que faz a filtragem total de todos os cursos
    
    A parte de adicionar a tabela ainda não funciona, perguntarei ao professor

--- main.py
+++ main.py
@@ -1,26 +1,87 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-import re
+import requests # Para requisições do site
+import pandas as pd # Para montar as tabelas (não utilizado nesse arquivos)
+from bs4 import BeautifulSoup # Para  poder manipular os dados dos sites 
+import re # Para poder cortar coisas desnecessárias do texto
+from unidecode import unidecode # Para poder ajustar o texto em algumas requisições
 
-# Criação do Testcode e Filtros
+# TESTE COM BS INICIO
+url= "https://www.pr.senac.br/psg_novo/partials/vagas/index.asp" # Url onde tem todas as Listas de cursos do Senac
 
-#linkdeanalises = 'https://www.pr.senac.br/psg/?p_psg=10&op=1&uc=1&tstcod=3211&tc=202400610'
+requrl = requests.get(url)
+soup = BeautifulSoup(requrl.text, "html.parser")
 
-tc = "202400503"
-tstcod = "3175"
+a = soup.find_all("a") # Os dados de Nomes de Filiais e Cursos estão alocados em <a>'s
 
-completo= "https://www.pr.senac.br/psg/?p_psg=10&op=1&uc=1&tstcod="+tstcod+"&tc="+tc
+inicio = 0 # Criação de Var Inicio
 
-# Extração de Dados em Planilha
-def extrair_tabela(html_table):
-    rows = html_table.find_all('tr')
-    data = []
-    for row in rows:
-        cols = row.find_all(["th", "td"])
-        cols = [col.text.strip() for col in cols]
-        data.append(cols)
-    return(data)
+# Isso é um filtro para localizar onde está Curitiba Centro (o inicio da Lista que vamos analisar)
+
+for i, a_tag in enumerate(a):
+    if "CURITIBA - CENTRO" in a_tag.get_text(strip=True):
+        inicio = i + 1 # Adicionando mais um por causa se esse é o Curitiba centro, o próximo é o primeiro curso da lista
+        break
+
+#Criando uma nova lista, para passar os valores a partir do nosso começo
+novalista = [] 
+
+for i in range(inicio, len(a)):
+    novalista.append(a[i])
+
+
+fimfake = 0 # Criação de Fim Fake (ela vai medir apenas o final para a nova lista)
+
+#Filtro fazendo o Unidecode limpar totalmente qualquer acento grafico (estava dando erro)
+for f, novalista_tag in enumerate(novalista):
+    if "CURITIBA - JARDIM BOTANICO" in unidecode(novalista_tag.get_text(strip=True)).upper():
+        fimfake = f - 1 # Mesma lógica, se a var é onde tem o texto, uma a menos é a ultima entrada do nosso intervalo de dados
+        break
+
+fim = inicio + fimfake # Pequena formula básica, para poder somar o Inicio de a até o fim de novalista, totalizando em fim de procura var
+
+# Agora nós temos duas vars Inicio e Fim, que são o tempo de intervalo que devemos percorrer
+
+# Essa primeira fórmula armazena o valor de TC de cada curso, cortando fora o que não é necessário
+
+tc = []
+n = inicio
+
+while n <= fim:
+    teste = a[n]
+    href_string = str(teste['href'])
+    numero = re.search(r'tc=(\d+)', href_string).group(1)
+    x = n
+    tc.append(numero)
+    n += 1
+
+# Agora possuindo tc, nós podemos ir em cada site, buscar o valor de tstcod que podemos encontar ao inspecionar
+# o elemento dá pagina e ver as turmas de cada curso
+
+tstcod = []
+
+for m in tc:
+    
+    urltest = ("https://www.pr.senac.br/cursos/?uep=1&tc="+m) # URL nova, m vai pegar o valor de cada digito de tc
+
+    tstcodurl= BeautifulSoup(requests.get(urltest).text, "html.parser")
+    tstcoda = tstcodurl.find_all("a",attrs={'href': lambda x: x and 'tstcod' in x}) # Esse é um pouco confuso, mas eu 
+    # descobri que dentre os <a>s dessa pagina, eles não eram padronizados, as vezes um poderia estar em tstcoda[6], as vezes em 
+    # tstcoda[2], por isso precisa de uma formula para pegar o <a> com tstcod
+
+    tstcody = tstcoda[0] # como estou usando find_all seria um array então eu só passei como var
+    
+    href_string2 = tstcody['href']
+    
+    tstcnumero = re.search(r'tstcod=(\d+)', href_string2).group(1) # corte para adicionar apenas os numeros
+    
+    tstcod.append(tstcnumero) # números adicionados
+# TESTE COM BS FIM
+
+tamanho = len(tc)
+
+for i in tamanho:
+    completo= (f"https://www.pr.senac.br/psg/?p_psg=10&op=1&uc=1&tstcod="+tstcod[i]+"&tc="+tc[i])
+
+print(completo)
 
 
 # Requisição do BS
@@ -42,6 +103,15 @@ processo_seletivo_regex = r'Processo Seletivo: <b>(\d+)</b>'
 processo_seletivo_match = re.search(processo_seletivo_regex, string_html)
 processo_seletivo = processo_seletivo_match.group(1) if processo_seletivo_match else None
 
+# Extração de Dados em Planilha
+def extrair_tabela(html_table):
+    rows = html_table.find_all('tr')
+    data = []
+    for row in rows:
+        cols = row.find_all(["th", "td"])
+        cols = [col.text.strip() for col in cols]
+        data.append(cols)
+    return(data)
 
 # Passando os dados do BS pra tabela
 data = extrair_tabela(tabelastestes[1])
--- testescombs.py
+++ testescombs.py
@@ -1,27 +1,50 @@
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-import re
-import time
+import requests # Para requisições do site
+import pandas as pd # Para montar as tabelas (não utilizado nesse arquivos)
+from bs4 import BeautifulSoup # Para  poder manipular os dados dos sites 
+import re # Para poder cortar coisas desnecessárias do texto
+from unidecode import unidecode # Para poder ajustar o texto em algumas requisições
 
-url= "https://www.pr.senac.br/psg_novo/partials/vagas/index.asp"
+url= "https://www.pr.senac.br/psg_novo/partials/vagas/index.asp" # Url onde tem todas as Listas de cursos do Senac
 
 requrl = requests.get(url)
 soup = BeautifulSoup(requrl.text, "html.parser")
-a = soup.find_all("a")
 
-unidade = a[91].get_text(strip=True)
+a = soup.find_all("a") # Os dados de Nomes de Filiais e Cursos estão alocados em <a>'s
 
-# Retorna Unidade Curitiba
+inicio = 0 # Criação de Var Inicio
 
-#criação de formula para armazenar "tc" em vars
+# Isso é um filtro para localizar onde está Curitiba Centro (o inicio da Lista que vamos analisar)
 
-# 92 = min, 142 max
-## CRIAÇÃO DO FOR
+for i, a_tag in enumerate(a):
+    if "CURITIBA - CENTRO" in a_tag.get_text(strip=True):
+        inicio = i + 1 # Adicionando mais um por causa se esse é o Curitiba centro, o próximo é o primeiro curso da lista
+        break
+
+#Criando uma nova lista, para passar os valores a partir do nosso começo
+novalista = [] 
+
+for i in range(inicio, len(a)):
+    novalista.append(a[i])
+
+
+fimfake = 0 # Criação de Fim Fake (ela vai medir apenas o final para a nova lista)
+
+#Filtro fazendo o Unidecode limpar totalmente qualquer acento grafico (estava dando erro)
+for f, novalista_tag in enumerate(novalista):
+    if "CURITIBA - JARDIM BOTANICO" in unidecode(novalista_tag.get_text(strip=True)).upper():
+        fimfake = f - 1 # Mesma lógica, se a var é onde tem o texto, uma a menos é a ultima entrada do nosso intervalo de dados
+        break
+
+fim = inicio + fimfake # Pequena formula básica, para poder somar o Inicio de a até o fim de novalista, totalizando em fim de procura var
+
+# Agora nós temos duas vars Inicio e Fim, que são o tempo de intervalo que devemos percorrer
+
+# Essa primeira fórmula armazena o valor de TC de cada curso, cortando fora o que não é necessário
+
 tc = []
-n = 92
+n = inicio
 
-while n <= 142:
+while n <= fim:
     teste = a[n]
     href_string = str(teste['href'])
     numero = re.search(r'tc=(\d+)', href_string).group(1)
@@ -29,17 +52,26 @@ while n <= 142:
     tc.append(numero)
     n += 1
 
-# Começa do 0 vai até 50
+# Agora possuindo tc, nós podemos ir em cada site, buscar o valor de tstcod que podemos encontar ao inspecionar
+# o elemento dá pagina e ver as turmas de cada curso
 
-tstcid = []
-m = 0
+tstcod = []
 
-idtc = str(tc[m])
-urltest = "https://www.pr.senac.br/cursos/?uep=1&tc="+idtc 
-tstcodurl= BeautifulSoup(requests.get(urltest).text, "html.parser")
-tstcoda = tstcodurl.find_all("a", target="_blank")
-tstc = tstcoda[6]
-href_string2 = str(tstc['href'])
-tstcnumero = re.search(r'tstcod=(\d+)', href_string2).group(1)
-tstcid.append(tstcnumero)
+for m in tc:
+    
+    urltest = ("https://www.pr.senac.br/cursos/?uep=1&tc="+m) # URL nova, m vai pegar o valor de cada digito de tc
 
+    tstcodurl= BeautifulSoup(requests.get(urltest).text, "html.parser")
+    tstcoda = tstcodurl.find_all("a",attrs={'href': lambda x: x and 'tstcod' in x}) # Esse é um pouco confuso, mas eu 
+    # descobri que dentre os <a>s dessa pagina, eles não eram padronizados, as vezes um poderia estar em tstcoda[6], as vezes em 
+    # tstcoda[2], por isso precisa de uma formula para pegar o <a> com tstcod
+
+    tstcody = tstcoda[0] # como estou usando find_all seria um array então eu só passei como var
+    
+    href_string2 = tstcody['href']
+    
+    tstcnumero = re.search(r'tstcod=(\d+)', href_string2).group(1) # corte para adicionar apenas os numeros
+    
+    tstcod.append(tstcnumero) # números adicionados
+
+    
